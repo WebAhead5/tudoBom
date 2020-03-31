6316cb62f53f71f2c901359f6d1b35909a0b56e8
Morad Abed <morad890@gmail.com>
2020-03-31 14:20:15 +0300

    adding the test to package.js

--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,14 @@
 var test = require('tape');
+// var tapSpec = require('tap-spec');
 var logic = require('../scripts/logic');
 
 test('Example test', function(t) {
-  t.pass();
+  // t.pass();
+  var actual = "1";
+  var expected = "1";
+  t.equal(actual, expected, "SOS")
   t.end();
 });
-
 
 
 
